584173450b21198924cdcac96041def4667bc565
Jason Kobuchi <jekobuchi@gmail.com>
2021-11-22 00:24:15 -0800

    Laid out skeleton, added some styling

---
+++ src/components/NFTCard.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './NFTCard.css';
+import equil from '../images/image-equilibrium.jpg';
+
+
+const NFTCard = () => (
+    <div className='main-card'>
+      <div className='image-container'>
+        <img src={equil} className='main-image'/>
+      </div>
+      <div className='mid-container'>
+        <div className='description'>
+          <h4>Equilibrium # 3429</h4>
+          <p>Our Equilibirum collection promotes balance and calm</p>
+        </div>
+        <div className='price-time'>
+          <p id='price'>0.041 ETH</p>
+          <p id='time'>3 days left</p>
+        </div>
+      </div>
+      <hr/>
+      <div className='bottom-container'>
+        <p>Creation of Jules Wyvern</p>
+      </div>
+    </div>
+);
+
+export default NFTCard;
